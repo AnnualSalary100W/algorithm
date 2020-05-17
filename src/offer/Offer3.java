df24052e9d7346435fd2aad02502567e35d6a887
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-17 19:48:23 +0800

    algorithm offer2

---
+++ src/offer/Offer3.java
@@ -0,0 +1,35 @@
+package offer;
+
+import java.util.ArrayList;
+
+
+public class Offer3 {
+
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode=new ListNode(1);
+        listNode.next = new ListNode(2);
+        ListNode t = listNode.next;
+        for(int i=3;i<10;i++) {//创建一个简单的链表{1,2,3,4,5,...,9}
+            t.next = new ListNode(i);
+            t = t.next;
+        }
+        System.out.println(printListFromTailToHead(listNode));
+    }
+        public  static  ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+            ArrayList<Integer> list =new ArrayList();
+            while(listNode!=null){
+                list.add(0,listNode.val);
+                listNode=listNode.next;
+            }
+            return list;
+        }
+
+}
