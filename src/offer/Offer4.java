e534b50680bb1b8ccb183da5aafd1b2522a1359d
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-18 11:32:57 +0800

    algorithm offer4

--- src/offer/Offer4.java
+++ src/offer/Offer4.java
@@ -1,6 +1,7 @@
 package offer;
 
 import java.util.Arrays;
+import java.util.*;
 
 public class Offer4 {
 
@@ -14,9 +15,9 @@ public class Offer4 {
       }
     public static void main(String[] args) {
 
-
+        System.out.println(new Offer4().reConstructBinaryTree(new int[]{1,2,4,7,3,5,6,8},new int[]{4,7,2,1,5,3,8,6}));
     }
-        public  static TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        public  TreeNode reConstructBinaryTree(int [] pre,int [] in) {
             if(pre.length==0||in.length==0){
                 return null;
             }
