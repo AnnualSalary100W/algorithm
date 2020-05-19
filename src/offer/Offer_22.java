3f65f641e919fd13976d87d7543d17ff5c50a929
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-19 22:52:01 +0800

    algorithm offer23

--- src/offer/Offer_22.java
+++ src/offer/Offer_22.java
@@ -4,6 +4,10 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Queue;
 
+/**
+ * 题目描述
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
 public class Offer_22 {
     public class TreeNode {
         int val = 0;
