70a383552a4eb2c4ac3d2395ccdf93884b4b9b75
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-19 11:45:00 +0800

    algorithm offer22

---
+++ src/offer/Offer_22.java
@@ -0,0 +1,37 @@
+package offer;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class Offer_22 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> result=new ArrayList<>();
+        if (root==null){
+        return  result;
+        }
+        Queue<TreeNode> queue=new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode node=queue.poll();
+            result.add(node.val);
+            if (node.left!=null){
+                queue.add(node.left);
+            }
+            if (node.right!=null){
+                queue.add(node.right);
+            }
+        }
+       return result;
+    }
+    }
+}
