466ef2744d514cef2ef6f9a58a69ca40c0eaece6
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-19 11:32:56 +0800

    algorithm offer21

--- src/offer/Offer_21.java
+++ src/offer/Offer_21.java
@@ -1,6 +1,35 @@
 package offer;
 
+import java.util.Stack;
+
 public class Offer_21 {
+    public static void main(String[] args) {
+        int[] pushA={1,2,3,4,5};
+        int[] popA={4,5,3,1,2};
+        System.out.println(IsPopOrder(pushA,popA));
+    }
 
-
+    public static boolean IsPopOrder(int [] pushA,int [] popA) {
+     if (pushA.length!=popA.length){
+         return false;
+     }
+     if (pushA.length<1){
+         return true;
+     }
+        Stack<Integer>stack=new Stack<>();
+     stack.push(pushA[0]);
+     int i=1;
+     for (int j=0;j<popA.length;j++){
+         int num =popA[j];
+         while (stack.peek()!=num&&i<pushA.length){
+             stack.push(pushA[i++]);
+         }
+         if (stack.peek()==num){
+             stack.pop();
+             continue;
+         }
+         return  false;
+     }
+     return true;
+    }
 }
