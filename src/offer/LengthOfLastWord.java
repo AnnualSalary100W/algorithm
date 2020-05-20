645d6e88a8f0f73d97c5cb009fed68225d749179
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-20 21:08:34 +0800

    algorithm LengthOfLastWord

---
+++ src/offer/LengthOfLastWord.java
@@ -0,0 +1,28 @@
+package offer;
+
+import java.util.Scanner;
+
+public class LengthOfLastWord {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        String line=sc.nextLine();
+
+        System.out.println(lengthOfLastWord(line));
+
+    }
+
+   public static int lengthOfLastWord(String s){
+        int count =0;
+        int index =s.length()-1;
+        while (index>=0&&s.charAt(index)==' '){
+            index--;
+        }
+        for (int i =index;i>=0;i--){
+            if(s.charAt(i)==' '){
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
