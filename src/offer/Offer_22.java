d46ad593f25369e1f278d0773f273e5c3603917f
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-21 22:24:05 +0800

    algorithm LengthOfLastWord

--- src/offer/Offer_22.java
+++ src/offer/Offer_22.java
@@ -8,7 +8,8 @@ import java.util.Queue;
  * 题目描述
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  */
-public class Offer_22 {
+class Offer_22 {
+
     public class TreeNode {
         int val = 0;
         TreeNode left = null;
@@ -18,7 +19,7 @@ public class Offer_22 {
             this.val = val;
 
         }
-    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+    public    ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
         ArrayList<Integer> result=new ArrayList<>();
         if (root==null){
         return  result;
@@ -37,5 +38,9 @@ public class Offer_22 {
         }
        return result;
     }
+
+
     }
+
+
 }
