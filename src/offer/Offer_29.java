9e30a23a3dc79649ff17bbe519b4904ab365c5a6
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-24 10:22:42 +0800

    输入n个整数，找出其中最小的K个数。 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。

--- src/offer/Offer_29.java
+++ src/offer/Offer_29.java
@@ -3,11 +3,8 @@ package offer;
 import java.util.ArrayList;
 import java.util.Arrays;
 /*
-求出1~13的整数中1出现的次数,
-并算出100~1300的整数中1出现的次数？
-为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,
-但是对于后面问题他就没辙了。ACMer希望你们帮帮他,并把问题更加普遍化,
-可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
+输入n个整数，找出其中最小的K个数。
+例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
  */
 public class Offer_29 {
     public static  ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
