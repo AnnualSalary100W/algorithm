0f38caa08e1fd94c0fed08757c7c5a4d9c559e98
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-24 10:10:46 +0800

    数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。 如果不存在则输出0。

---
+++ src/offer/Offer_28.java
@@ -0,0 +1,33 @@
+package offer;
+
+import java.util.Arrays;
+
+/*
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。
+如果不存在则输出0。
+ */
+public class Offer_28 {
+    public static int MoreThanHalfNum_Solution(int [] array) {
+        int count=0;
+        Arrays.sort(array);
+        int mid=array.length/2;
+        for(int i=0;i<array.length;i++){
+          if (array[i]==array[mid]){
+              count++;
+          }
+        }
+if (count>mid){
+    return array[mid];
+}else {
+    return 0;
+}
+
+    }
+
+    public static void main(String[] args) {
+        int [] a = new int[]{1,2,2,4,2};
+        System.out.println(MoreThanHalfNum_Solution(a));
+    }
+}
