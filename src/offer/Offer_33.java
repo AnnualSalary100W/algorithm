2f3cdfc464940a21c2ad54478b927cbe02cdd975
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-25 10:56:08 +0800

    把只包含质因子2、3和5的数称作丑数（Ugly Number）。 例如6、8都是丑数，但14不是，因为它包含质因子7。 习惯上我们把1当做是第一个丑数。 求按从小到大的顺序的第N个丑数。

--- src/offer/Offer_33.java
+++ src/offer/Offer_33.java
@@ -1,5 +1,9 @@
 package offer;
-
+/*
+把只包含质因子2、3和5的数称作丑数（Ugly Number）。
+例如6、8都是丑数，但14不是，因为它包含质因子7。
+ 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
+ */
 public class Offer_33 {
     public int GetUglyNumber_Solution(int index) {
         int[] a=new int[index];
