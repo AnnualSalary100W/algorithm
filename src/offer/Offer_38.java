77433390e941700bd63cd5363b1cf2b09f43f9ba
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-26 10:26:35 +0800

    输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。

---
+++ src/offer/Offer_38.java
@@ -0,0 +1,28 @@
+package offer;
+
+
+public class Offer_38 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    public int TreeDepth(TreeNode root) {
+if (root==null){
+    return 0;
+}
+int leftDepth=TreeDepth(root.left);
+int rightDepth=TreeDepth(root.right);
+return Math.max(leftDepth+1,rightDepth+1);
+
+
+
+    }
+}
