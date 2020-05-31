63fbb9181b4e1df69cd0c528473235010278bbd5
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-05-31 09:16:58 +0800

    写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。

---
+++ src/offer/Offer_48.java
@@ -0,0 +1,10 @@
+package offer;
+
+public class Offer_48 {
+    public static void main(String[] args) {
+        System.out.println(Add(13,2));
+    }
+    public static int Add(int num1,int num2) {
+return Math.addExact(num1,num2);
+    }
+}
