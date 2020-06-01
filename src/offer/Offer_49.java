b137e61b9bcb6e8b355c1217ddff7b27382346ee
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-01 10:48:43 +0800

    .

---
+++ src/offer/Offer_49.java
@@ -0,0 +1,49 @@
+package offer;
+
+public class Offer_49 {
+    public int StrToInt(String str) {
+       int len=str.length();
+       int index=0;
+       //第一步，删除前面的空格
+        while (index<len){
+            if (str.charAt(index)==' '){
+                index++;
+            }else {
+                break;
+            }
+        }
+        int flag=0;
+        long ans=0;//最终返回结果
+        while (index<len){
+            if (str.charAt(index)=='-'||str.charAt(index)=='+'){
+                if (flag!=0){
+                    return  0;//"-123-1",第二个-是非法字符
+                }
+                flag=str.charAt(index)=='-'?-1:1;
+            }else if (str.charAt(index)>='0'&&str.charAt(index)<='9'){
+                ans=ans*10+(str.charAt(index)-'0');
+                if (judge(ans,flag)){
+return Integer.parseInt(str);
+                }
+            }else {
+                return 0;
+            }
+            index++;
+        }
+return flag==-1?(int)ans*(-1):(int)ans;
+    }
+
+    private static boolean judge(long ans, int flag) {
+        if (flag==-1){
+            if (ans*(-1)<Integer.MIN_VALUE){
+                return true;
+            }
+            return false;
+        }else {
+            if (ans>Integer.MAX_VALUE){
+                return true;
+            }
+            return false;
+        }
+    }
+}
