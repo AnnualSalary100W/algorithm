02061ac53be6926a4a5bc06f386a0bfe791d183a
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-04 09:38:58 +0800

    .

---
+++ src/offer/Offer_58.java
@@ -0,0 +1,29 @@
+package offer;
+
+public class Offer_58 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot == null) return true;
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+    private boolean isSymmetrical(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return left.val == right.val //为镜像的条件：左右节点值相等
+                && isSymmetrical(left.left, right.right) //2.对称的子树也是镜像
+                && isSymmetrical(left.right, right.left);
+    }
+}
