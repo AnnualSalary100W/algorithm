a4163ae0a740e30a3f54b482c069fc82e678ef88
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-06 10:24:37 +0800

    .

---
+++ src/offer/Offer_63.java
@@ -0,0 +1,54 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Offer_63 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> list1=new  ArrayList<ArrayList<Integer>>();
+        if (pRoot == null) {
+            return list1;
+        }
+        ArrayList<Integer> list2=new ArrayList<Integer>();
+        Queue<TreeNode> queue=new LinkedList<TreeNode>();
+
+        int now=1;
+        int next=0;
+        queue.offer(pRoot);
+
+        while (!queue.isEmpty()){
+            TreeNode t=queue.poll();
+            list2.add(t.val);
+            now--;
+            if (t.left!=null){
+                queue.offer(t.left);
+                next++;
+            }
+            if (t.right!=null){
+                queue.offer(t.right);
+                next++;
+            }
+            if (now==0){
+                list1.add(list2);
+                list2=new ArrayList<>();
+                now=next;
+                next=0;
+            }
+        }
+        return list1;
+
+    }
+
+}
