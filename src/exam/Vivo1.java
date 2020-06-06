fc99ca8a0361a02c51998b763d23110706ad92a2
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-06 22:41:19 +0800

    在vivo产线上，每位职工随着对手机加工流程认识的熟悉和经验的增加，日产量也会不断攀升。 假设第一天量产1台，接下来2天(即第二、三天)每天量产2件，接下来3天(即第四、五、六天)每天量产3件 ... ... 以此类推，请编程计算出第n天总共可以量产的手机数量。

--- src/exam/Vivo1.java
+++ src/exam/Vivo1.java
@@ -1,5 +1,11 @@
 package exam;
 import java.util.*;
+/*
+在vivo产线上，每位职工随着对手机加工流程认识的熟悉和经验的增加，日产量也会不断攀升。
+假设第一天量产1台，接下来2天(即第二、三天)每天量产2件，接下来3天(即第四、五、六天)每天量产3件 ... ...
+以此类推，请编程计算出第n天总共可以量产的手机数量。
+ */
+
 public class Vivo1 {
 
     public static void main(String[] args) {
