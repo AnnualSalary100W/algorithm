5a4d584ac1198611cd2e047b8d618bfc1b8b2723
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-06 10:25:04 +0800

    从上到下按层打印二叉树，同一层结点从左至右输出。 每一层输出一行。

--- src/offer/Offer_63.java
+++ src/offer/Offer_63.java
@@ -3,7 +3,10 @@ package offer;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-
+/*
+从上到下按层打印二叉树，同一层结点从左至右输出。
+每一层输出一行。
+ */
 public class Offer_63 {
     public class TreeNode {
         int val = 0;
