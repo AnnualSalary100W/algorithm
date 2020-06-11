0f773a72a57bd0668b9570275d895d8e9eced1cc
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-11 09:39:19 +0800

    根据每日 气温 列表，请重新生成一个列表，对应位置的输出是需要再等待多久温度才会升高超过该日的天数。如果之后都不会升高，请在该位置用 0 来代替。 例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。

---
+++ src/leetcode/DailyTemperatures.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class DailyTemperatures {
+    public static void main(String[] args) {
+        int[] T=new  int[]{73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(dailyTemperatures(T));
+    }
+    public  static int[] dailyTemperatures(int[] T) {
+             int n=T.length;
+             int[] arr=new int[n];
+
+             for (int i=0;i<n;i++){
+                 int flag=0;
+                 for (int j=i+1;j<n;j++){
+                     if (T[i] < T[j]) {
+                        arr[i]= j-i;
+                         flag=1;
+                        break;
+                     }
+                 }
+                 if (flag==0){
+                     arr[i]=0;
+                 }
+
+             }
+             return arr;
+    }
+}
