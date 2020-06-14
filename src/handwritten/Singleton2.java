5fbf7d0b0c4b8b4c9a6275b178c33969e71801c1
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-14 17:37:59 +0800

    DCL

---
+++ src/handwritten/Singleton2.java
@@ -0,0 +1,16 @@
+package handwritten;
+
+public class Singleton2 {
+    private volatile static Singleton2 singleton2;
+    private Singleton2(){}
+    public static  Singleton2 getSingleton2(){
+        if (singleton2==null){
+            synchronized (Singleton2.class){
+                if (singleton2==null){
+                    singleton2=new Singleton2();
+                }
+            }
+        }
+        return singleton2;
+    }
+}
