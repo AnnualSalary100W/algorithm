52e2d2692fc4371d557781ae46a1c21fe5b33379
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-14 11:01:42 +0800

    给你一个整数数组 arr 和一个目标值 target ，请你返回一个整数 value ，使得将数组中所有大于 value 的值变成 value 后，数组的和最接近  target （最接近表示两者之差的绝对值最小）。 如果有多种使得和最接近 target 的方案，请你返回这些整数中的最小值。 请注意，答案不一定是 arr 中的数字。

---
+++ src/leetcode/FindBestValue.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class FindBestValue {
+    public static void main(String[] args) {
+        int[] arr=new int[]{4,9,3};
+        int target=10;
+        System.out.println(findBestValue(arr,target));
+    }
+    public  static int findBestValue(int[] arr, int target) {
+        Arrays.sort(arr);
+        int len=arr.length;
+        for(int i=0;i<len;i++){
+            if(arr[i]*(len-i)>target){
+                int ans=target/(len-i);
+                return Math.abs(ans*(len-i)-target)>Math.abs((ans+1)*(len-i)-target)?ans+1:ans;
+            }
+            else{
+                target-=arr[i];
+            }
+        }
+        return arr[len-1];
+
+    }
+}
