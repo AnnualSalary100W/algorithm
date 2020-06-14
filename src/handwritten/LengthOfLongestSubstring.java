3c32e03972bddf233b351a0ec18fb64cb2a2f78d
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-14 17:58:50 +0800

    请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。

--- src/handwritten/LengthOfLongestSubstring.java
+++ src/handwritten/LengthOfLongestSubstring.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+示例 1:
+输入: "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ */
 public class LengthOfLongestSubstring {
 
     public static void main(String[] args) {
