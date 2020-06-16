74076520b8d9b2ec6a41e555de5331e6cff333b2
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-16 22:44:44 +0800

    给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -3,7 +3,10 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+/*
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c
+使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ */
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums=new int[]{-1, 0, 1, 2, -1, -4};
