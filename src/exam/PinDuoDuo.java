c15aaac95140e92caf8b4ad08a6fc3e47a42c3d9
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-17 11:15:52 +0800

    在一块长为n，宽为m的场地上，有n✖️m个1✖️1的单元格。每个单元格上的数字就是按照从1到n和1到m中的数的乘积. 给出一个查询的值k，求出按照这个方式列举的的数中第k大的值v。

--- src/exam/PinDuoDuo.java
+++ src/exam/PinDuoDuo.java
@@ -3,7 +3,10 @@ package exam;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
-
+/*
+在一块长为n，宽为m的场地上，有n✖️m个1✖️1的单元格。每个单元格上的数字就是按照从1到n和1到m中的数的乘积.
+ 给出一个查询的值k，求出按照这个方式列举的的数中第k大的值v。
+ */
 public class PinDuoDuo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
