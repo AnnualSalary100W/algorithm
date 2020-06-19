61cc45bc3bdbd55c9bb9a60f90819aa5535708fc
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-19 15:14:41 +0800

    给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。

--- src/leetcode/IsPalindrome1.java
+++ src/leetcode/IsPalindrome1.java
@@ -1,12 +1,15 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 /*
 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
  */
 public class IsPalindrome1 {
+
     public static void main(String[] args) {
+
         Scanner scanner=new Scanner(System.in);
         String s=scanner.nextLine();
         System.out.println(isPalindrome(s));
