2bf2dc19044b580304a484819c3e97bb0de6c750
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-25 09:37:18 +0800

    给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。 说明： 拆分时可以重复使用字典中的单词。 你可以假设字典中没有重复的单词。

--- src/leetcode/WordBreak.java
+++ src/leetcode/WordBreak.java
@@ -3,7 +3,13 @@ package leetcode;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+/*
+给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+说明：
+拆分时可以重复使用字典中的单词。
+你可以假设字典中没有重复的单词。
 
+ */
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> wordDictSet = new HashSet(wordDict);
