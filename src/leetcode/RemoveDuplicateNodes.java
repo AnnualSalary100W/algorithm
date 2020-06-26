d71e8400b287008db753171e090e7f0b555cacde
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-26 12:20:12 +0800

    移除重复节点

---
+++ src/leetcode/RemoveDuplicateNodes.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class RemoveDuplicateNodes {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        if (head == null) {
+            return head;
+        }
+        ListNode temp=head;
+        hashSet.add(head.val);
+        while (temp.next!=null){
+            ListNode cur = temp.next;
+        if (hashSet.add(cur.val)){
+            temp=temp.next;
+        }else {
+            temp.next=temp.next.next;
+        }
+
+        }
+        temp.next=null;
+        return head;
+    }
+}
