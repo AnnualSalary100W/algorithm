5ce3c17b436032041142140a2a13e58e7f9deb66
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-27 22:52:11 +0800

    从1开始先丢失的数

---
+++ src/leetcode/FirstMissingPositive.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums=new int[]{1,2,0};
+        System.out.println(firstMissingPositive(nums));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        int n=nums.length;
+        int m=1;
+        for (int i=0;i<n;i++){
+            hashMap.put(i,nums[i]);
+        }
+        for (int j=1;j<=n+1;j++){
+            if (hashMap.containsValue(j)){
+                continue;
+            }else {
+                m=j;
+                break;
+            }
+        }
+        return m;
+    }
+}
