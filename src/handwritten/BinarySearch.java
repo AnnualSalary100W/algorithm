5b467b71e12763d47eac5aec895d31ab71a28e04
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-27 11:34:32 +0800

    二分查找

---
+++ src/handwritten/BinarySearch.java
@@ -0,0 +1,27 @@
+package handwritten;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+               int[] arr=new int[]{1,2,3,4,5,8,9,45,80};
+               int key =2;
+        System.out.println(BinarySearch(arr,key));
+    }
+    public static int BinarySearch(int[] arr,int key){
+        int low=0;
+        int high=arr.length-1;
+        if (low>high||key<arr[low]||key>arr[high]){
+            return -1;
+        }
+        while (low<high){
+            int middle=low+(high-low)/2;
+            if (arr[middle]>key){
+                high=middle-1;
+            }else if (arr[middle]<key){
+                low=middle+1;
+            }else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
