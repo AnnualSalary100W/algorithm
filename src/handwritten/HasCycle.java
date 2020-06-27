1e06b0453f5f75fd1f7b9766a8ea5bcd3e692b91
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-06-27 11:34:17 +0800

    判断单链表有环

---
+++ src/handwritten/HasCycle.java
@@ -0,0 +1,24 @@
+package handwritten;
+
+public class HasCycle {
+    class ListNode{
+        int val;
+        ListNode next;
+      ListNode (int x){
+          val=x;
+          next=null;
+      }
+    }
+
+
+    public boolean hasCycle(ListNode head){
+        ListNode first=head;
+        ListNode slow=head;
+        while (first!=null&&first.next!=null){
+            first=first.next.next;
+            slow=slow.next;
+            if (first==slow){return true;}
+        }
+        return false;
+    }
+}
