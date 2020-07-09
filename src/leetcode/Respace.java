1280181c1b4fb40d88d7e62a8c763f504e625c10
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-09 09:38:54 +0800

    恢复空格

--- src/leetcode/Respace.java
+++ src/leetcode/Respace.java
@@ -1,7 +1,13 @@
 package leetcode;
 
 import java.util.Arrays;
+/*
+哦，不！你不小心把一个长篇文章中的空格、标点都删掉了，并且大写也弄成了小写。
+句子"I reset the computer. It still didn’t boot!"已经变成了"iresetthecomputeritstilldidntboot"。
+在处理标点符号和大小写之前，你得先把它断成词语。当然了，你有一本厚厚的词典dictionary，
+不过，有些词没在词典里。假设文章用sentence表示，设计一个算法，把文章断开，要求未识别的字符最少，返回未识别的字符数
 
+ */
 public class Respace {
     public int respace(String[] dictionary, String sentence) {
         int n = sentence.length();
