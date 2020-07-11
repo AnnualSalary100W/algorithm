43d34322dd41af885fdc1b796ab9eee4ed652ec4
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-11 08:41:11 +0800

    315. 计算右侧小于当前元素的个数

--- src/leetcode/CountSmaller315.java
+++ src/leetcode/CountSmaller315.java
@@ -5,7 +5,11 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+/*
+给定一个整数数组 nums，按要求返回一个新数组 counts。
+数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于 nums[i] 的元素的数量。
 
+ */
 public class CountSmaller315 {
     public List<Integer> countSmaller(int[] nums) {
         if(nums == null || nums.length == 0) return new LinkedList<>();
