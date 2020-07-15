ec0feb080ca46d679b4b51a694169d1537cebfe5
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-15 09:15:41 +0800

    96. 不同的二叉搜索树

---
+++ src/leetcode/NumTrees.java
@@ -0,0 +1,13 @@
+package leetcode;
+/*
+96. 不同的二叉搜索树
+ */
+public class NumTrees {
+    public int numTrees(int n) {
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+}
