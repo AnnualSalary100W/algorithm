31d33f0460d8004f8102d7b4dd91ee5d464d19a4
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-19 09:25:00 +0800

    312. 戳气球

--- src/leetcode/MaxCoins312.java
+++ src/leetcode/MaxCoins312.java
@@ -5,6 +5,7 @@ package leetcode;
 现在要求你戳破所有的气球。如果你戳破气球 i ，就可以获得 nums[left] * nums[i] * nums[right] 个硬币。 
 这里的 left 和 right 代表和 i 相邻的两个气球的序号。注意当你戳破了气球 i 后，气球 left 和气球 right 就变成了相邻的气球。
 求所能获得硬币的最大数量。
+
  */
 public class MaxCoins312 {
     public int maxCoins(int[] nums) {
