ab551610be6cb40d7da4bab09a1c69ba5758b513
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-20 16:47:38 +0800

    167. 两数之和 II - 输入有序数组

---
+++ src/leetcode/TwoSum167.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class TwoSum167 {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        int[] arr=new int[2];
+        int n=numbers.length;
+        for(int i=0;i<n;i++){
+            if (hashMap.containsKey(numbers[i])){
+                for (int j=0;j<i;j++){
+                    if (numbers[j]==hashMap.get(numbers[i]))
+                   arr[0]=j+1;
+                }
+                arr[1]=i+1;
+            }else {hashMap.put(target-numbers[i],numbers[i]);}
+        }
+        return  arr;
+    }
+}
