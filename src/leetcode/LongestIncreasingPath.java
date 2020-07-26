7f8e24857738aecad94954a0b6580d67ffd43fe6
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-07-26 17:57:01 +0800

    给定一个整数矩阵，找出最长递增路径的长度。 对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）

--- src/leetcode/LongestIncreasingPath.java
+++ src/leetcode/LongestIncreasingPath.java
@@ -1,5 +1,7 @@
 package leetcode;
-
+/*
+对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+*/
 public class LongestIncreasingPath {
     public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public int rows, columns;
