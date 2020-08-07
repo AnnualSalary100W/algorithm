c146c971f73b6336b7e557c4dc91893f6a5201df
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-08-07 09:22:18 +0800

    100. 相同的树

---
+++ src/leetcode/IsSameTree100.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class IsSameTree100 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if(q==null&&p==null){
+            return true;
+        } else if ((q==null&p!=null)|(p==null&q!=null)){
+            return  false;
+        }else if (p.val != q.val) {
+            return false;}
+
+        else{ return  isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+
+        }
+
+
+    }
+
+
+
+
+}
