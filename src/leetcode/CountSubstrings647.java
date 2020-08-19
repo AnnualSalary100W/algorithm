a735054f3491929474e1e8e4b7470c4006c8f4c0
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-08-19 10:18:22 +0800

    647.给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。

---
+++ src/leetcode/CountSubstrings647.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class CountSubstrings647 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(countSubstrings(s));
+    }
+    public  static int countSubstrings(String s) {
+        int res = 0;
+        int n = s.length();
+
+        // dp[i][j] 表示[i,j]的字符是否为回文子串
+        boolean[][] dp = new boolean[n][n];
+
+        // 注意，外层循环要倒着写，内层循环要正着写
+        // 因为要求dp[i][j] 需要知道dp[i+1][j-1]
+        for(int i=n-1; i>=0; i--){
+            for(int j=i; j<n; j++){
+
+                // (s.charAt(i)==s.charAt(j) 时，当元素个数为1,2,3个时，一定为回文子串
+                if(s.charAt(i)==s.charAt(j) && (j-i<=2 || dp[i+1][j-1])){
+                    dp[i][j] = true;
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
