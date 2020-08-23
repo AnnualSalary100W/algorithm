c815f0b6c29725903538b3b35167ed140271a7f4
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-08-23 14:42:09 +0800

    201. 数字范围按位与

---
+++ src/leetcode/RangeBitwiseAnd201.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class RangeBitwiseAnd201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        while (m < n) {
+            // 抹去最右边的 1
+            n = n & (n - 1);
+        }
+        return n;
+    }
+}
