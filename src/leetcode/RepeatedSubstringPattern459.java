c5c6797443e49d28522d89cd6302495b53ea5d25
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-08-24 09:16:54 +0800

    459. 重复的子字符串

---
+++ src/leetcode/RepeatedSubstringPattern459.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+
+import java.util.Scanner;
+
+public class RepeatedSubstringPattern459 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+        System.out.println(repeatedSubstringPattern(s));
+    }
+    public static boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i * 2 <= n; ++i) {
+            if (n % i == 0) {
+                boolean match = true;
+                for (int j = i; j < n; ++j) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
