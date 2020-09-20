e81f8544789c365c6c06924c3ec2edf16f263f77
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-09-20 14:11:35 +0800

    美团第二题

---
+++ src/exam/meituan1/Main2.java
@@ -0,0 +1,10 @@
+package exam.meituan1;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(40);
+    }
+}
