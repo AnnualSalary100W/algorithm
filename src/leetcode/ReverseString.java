b57781b2974d2f600388ab92167df92ec1ec8db4
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-10-08 11:40:13 +0800

    344. 反转字符串

---
+++ src/leetcode/ReverseString.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        for(int left = 0,right = n - 1; left < right; left++,right--){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+        }
+    }
+}
