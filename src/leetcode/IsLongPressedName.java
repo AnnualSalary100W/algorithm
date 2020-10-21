59c3c8943c7118ccc2656322f43de8fd6a2249c5
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-10-21 11:33:48 +0800

    925. 长按键入

---
+++ src/leetcode/IsLongPressedName.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class IsLongPressedName {
+    public static void main(String[] args) {
+
+
+
+        String s1 =  "kikcxmvzi";
+        String s2 =  "kiikcxxmmvvzz";
+        System.out.println(isLongPressedName(s1,s2));
+    }
+    private static boolean isLongPressedName(String name, String typed) {
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+}
