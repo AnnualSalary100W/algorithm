997302af0a481efc0f4aa963eeb141de9745c8b3
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-12-13 15:59:29 +0800

    AnnualSalary100W

--- src/leetcode/LemonadeChange.java
+++ src/leetcode/LemonadeChange.java
@@ -1,8 +1,8 @@
 package leetcode;
 
 public class LemonadeChange {
-
     public boolean lemonadeChange(int[] bills) {
+
         int five = 0, ten = 0;
         for (int bill : bills) {
             if (bill == 5) {
