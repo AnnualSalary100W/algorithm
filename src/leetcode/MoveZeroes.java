2731d7cfdcdec33b1f4fee221230626580755314
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2020-12-16 18:24:57 +0800

    AnnualSalary100W

--- src/leetcode/MoveZeroes.java
+++ src/leetcode/MoveZeroes.java
@@ -6,7 +6,6 @@ public class MoveZeroes {
     public static void main(String[] args) {
 
     }
-
         public void moveZeroes(int[] nums) {
             int j = 0;
             for (int i = 0; i < nums.length; i++ ){
