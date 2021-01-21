860ad220da1c31dd002a7988607ef138b2ef7d0f
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2021-01-21 17:34:16 +0800

    Update CountSubstrings647.java

--- src/leetcode/CountSubstrings647.java
+++ src/leetcode/CountSubstrings647.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.util.Scanner;
 
 public class CountSubstrings647 {
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.next();
