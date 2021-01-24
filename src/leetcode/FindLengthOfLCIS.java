0489a51740a43b806a2b5fd54c1c7913a5f2da47
AnnualSalary100W <57079809+AnnualSalary100W@users.noreply.github.com>
2021-01-24 13:59:34 +0800

    674. 最长连续递增序列

---
+++ src/leetcode/FindLengthOfLCIS.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class FindLengthOfLCIS {
+    public static void main(String[] args) {
+         int[] arr= new int[]{2,2,2,2,2};
+        System.out.println(findLengthOfLCIS(arr));
+    }
+    public static int findLengthOfLCIS(int[] nums) {
+         int n = nums.length;
+         if (n == 0) return 0;
+         int maxNumber = 1;
+         int res = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                maxNumber++;
+                res = Math.max( res , maxNumber);
+            }else {
+                maxNumber = 1;
+            }
+        }
+        return res;
+    }
+}
